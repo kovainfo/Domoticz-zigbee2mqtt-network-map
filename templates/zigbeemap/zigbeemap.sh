20d5be806f18aeca25acbc8e1d785fcc97dc1d0a
kovainfo <36986231+kovainfo@users.noreply.github.com>
2020-04-06 09:54:30 +0300

    path to json as parameter

--- templates/zigbeemap/zigbeemap.sh
+++ templates/zigbeemap/zigbeemap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-mosquitto_sub -t zigbee2mqtt/bridge/networkmap/raw -C 1 > /home/pi/domoticz/www/templates/zigbeemap/raw.json &
+dir=$1
+
+mosquitto_sub -t zigbee2mqtt/bridge/networkmap/raw -C 1 > "$dir"raw.json &
 mosquitto_pub -t zigbee2mqtt/bridge/networkmap -m raw
 
 # wait until mosquitto_sub ends
